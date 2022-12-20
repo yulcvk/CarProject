7599b0ad0305921a6443bdcf3eb776a9890379fe
yulcvk <yulia.chevik@gmail.com>
2022-12-19 21:14:32 -0500

    the features were added

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
 
         CarFeatures XLR=new CarFeatures();
         System.out.println("launch car");
+        XLR.color="red";
+        XLR.doors="5";
+        XLR.engine=3456;
+        XLR.model="a5";
     }
 }
